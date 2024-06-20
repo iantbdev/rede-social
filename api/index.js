d4eed92affaa539c3181400b43b1925f8c735ee6
ian <108193413+iantbdev@users.noreply.github.com>
2024-06-20 11:45:41 -0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import userRoutes from "./routes/users.js";
+import postsRoutes from "./routes/posts.js";
+import likesRoutes from "./routes/likes.js";
+import commentsRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+
+//middlewares
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postsRoutes);
+app.use("/api/likes", likesRoutes);
+app.use("/api/comments", commentsRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(8800, () => {
+  console.log("API funciona!");
+});
