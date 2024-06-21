f8ddaf5966f041ab55408191724455ea56840fc1
Jorgelino328 <jorgewilliam328@gmail.com>
2024-06-21 01:38:16 -0300

    feat: adicionar inicio de chamadas spotify
    tambem coloquei um player no home so pra testar msm e vcs verem

--- api/connect.js
+++ api/connect.js
@@ -3,6 +3,6 @@ import mysql2 from "mysql2";
 export const db = mysql2.createConnection({
   host: "localhost",
   user: "root",
-  password: "123",
+  password: "senha",
   database: "redesocial",
 });
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Stories from "../../components/stories/Stories";
 import Posts from "../../components/posts/Posts";
 import Share from "../../components/share/Share";
 import "./home.scss";
+import MusicPlayer from "../../components/musicPlayer/musicPlayer";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Stories />
       <Share />
       <Posts />
+      <MusicPlayer />
     </div>
   );
 };
