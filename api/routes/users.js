08a4ccf0263bdefb2a0cb5f734f30d6c360b8711
Gustavobiz <gustavobernardes05@hotmail.com>
2024-06-24 05:33:42 -0300

    estabelecendo rotas

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUser, getSuggestions } from "../controllers/userController.js";
+import { getUser, getSuggestions, getUsersByName } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/find/:id", getUser);
+router.get("/findByName", getUsersByName);
 router.get("/suggestions/:id", getSuggestions);
 export default router;
